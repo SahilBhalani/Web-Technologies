a78dbf5c60af339f209d59f9d62f4b6af3bc7b36
Sahil <sahilbhalani007@gmail.com>
2025-11-20 09:56:05 +0530

    JS Module Import Export Progs Added

---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/02_Named_Export/app.js
@@ -0,0 +1,8 @@
+//Import Specific function
+import {add, subtract, multiply, divide} from './arithmetic.js';
+
+//Use the imported functions
+console.log(add(10,5));
+console.log(subtract(10,5));
+console.log(multiply(10,5));
+console.log(divide(10,5));
---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/02_Named_Export/arithmetic.js
@@ -0,0 +1,16 @@
+//Export named function for arithemetic operations
+export function add(a,b) {
+    return a + b;
+}
+export function subtract(a,b) {
+    return a - b;
+}
+export function multiply(a,b) {
+    return a * b;
+}
+export function divide(a,b) {
+    if(b === 0) {
+        throw new Error('Division by zero is not allowed');
+    }
+    return a / b;
+}
