cc302a2fcae54a5bc7bfa6cf18b33af97f66de00
Sahil <sahilbhalani007@gmail.com>
2025-11-20 09:57:19 +0530

    JS Module Import Export Progs Added

---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/09_Exporting_Constants/constants.js
@@ -0,0 +1,6 @@
+//Exporting multiple constantd, including a defualt export
+const GRAVITY = 9.81;
+export const LIGHT_SPEED = 299792458;
+export const PLANCK = 6.626e-34;
+
+export default GRAVITY;
---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/09_Exporting_Constants/main.js
@@ -0,0 +1,7 @@
+//Importing the default constant and named exports
+import GRAVITY, { LIGHT_SPEED , PLANCK} from './constants.js'
+
+//Use the important constants
+console.log(`Value of Gravity: ${GRAVITY}`);
+console.log(`Speed of Light: ${LIGHT_SPEED}`);
+console.log(`Planck's Constant: ${PLANCK}`);
