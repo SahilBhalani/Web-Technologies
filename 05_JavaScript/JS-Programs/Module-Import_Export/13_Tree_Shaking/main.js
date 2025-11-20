d4ae3ca93d80a32802e641b97bc58e8497da5add
Sahil <sahilbhalani007@gmail.com>
2025-11-20 09:57:55 +0530

    JS Module Import Export Progs Added

---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/13_Tree_Shaking/main.js
@@ -0,0 +1,7 @@
+//importing the default export and desturcturing only the required
+import mathUtils from "./mathUtils.js";
+const { add , divide} = mathUtils;
+
+//Using the functions
+console.log(add(8,2));
+console.log(divide(20,4));
---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/13_Tree_Shaking/mathUtils.js
@@ -0,0 +1,9 @@
+//Exporting a utility object containing multiple functions
+const mathUtils = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+
+export default mathUtils;
