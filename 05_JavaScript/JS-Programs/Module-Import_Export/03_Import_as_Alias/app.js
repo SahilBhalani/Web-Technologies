fa85cd7bb42f230057ccc38aaf24f9bbe772c0dc
Sahil <sahilbhalani007@gmail.com>
2025-11-20 09:56:12 +0530

    JS Module Import Export Progs Added

---
+++ 05_JavaScript/JS-Programs/Module-Import_Export/03_Import_as_Alias/app.js
@@ -0,0 +1,7 @@
+//* Import all exports as an alias
+import * as utils from './utilities.js'
+
+//Use the imported properties
+console.log(utils.capitalize('hello'));
+console.log(utils.reverse('world'));
+console.log(utils.toLowerCase('TESTTT'));
